4acc14e80ac692aecc635d698595590df7e08692
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-08-29 17:00:43 -0300

    feat: menu - tabela de validacoes

--- app/src/main/java/br/com/fiap/myapp/screens/MenuScreen.kt
+++ app/src/main/java/br/com/fiap/myapp/screens/MenuScreen.kt
@@ -1,12 +1,15 @@
 package br.com.fiap.myapp.screens
 
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,15 +17,20 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -44,7 +52,8 @@ fun MenuScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(Color.Blue)
-                    .padding(vertical = 54.dp)
+                    .padding(top = 54.dp)
+                    .padding(bottom = 27.dp)
                     .padding(horizontal = 36.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
@@ -63,14 +72,16 @@ fun MenuScreen() {
                         painter = painterResource(id = R.drawable.user),
                         contentDescription = "Ícone do usuário",
                         Modifier
-                            .size(56.dp)
-                            .padding(end = 12.dp)
+                            .size(48.dp)
+                            .padding(end = 0.dp)
                     )
 
                     Image(
                         painter = painterResource(id = R.drawable.options),
                         contentDescription = "Ícone de opções",
-                        Modifier.size(24.dp)
+                        Modifier
+                            .size(24.dp)
+                            .padding(start = 18.dp)
                     )
                 }
             }
@@ -103,6 +114,8 @@ fun MenuScreen() {
                     fontSize = 24.sp,
                     modifier = Modifier.padding(16.dp))
 
+                TableScreen()
+
             }
         }
     }
@@ -126,6 +139,59 @@ fun CardMaker(name: String, color: Color, dp: Dp) {
             color = Color.White,
             modifier = Modifier.padding(12.dp))
 
+    }
+}
+
+@Composable
+fun RowScope.TableCell(
+    content: Any?,
+    weight: Float
+) {
+    when (content) {
+        is String -> Text(
+            text = content,
+            Modifier
+                .weight(weight)
+                .padding(8.dp))
+
+        is Painter -> Icon(
+            painter = content,
+            contentDescription = "Ícone de informações",
+            Modifier
+                .size(36.dp)
+                .weight(weight)
+                .padding(8.dp)
+        )
+    }
+
+}
+
+@Composable
+fun TableScreen() {
+    val tableData = (1..10).mapIndexed { index, item ->
+        index to "Item $index"
+    }
+    val columnWeight = .25f
+    LazyColumn(
+        Modifier
+            .fillMaxSize()) {
+        item {
+            Row() {
+                TableCell("", columnWeight)
+                TableCell("Data", columnWeight)
+                TableCell("Nome", columnWeight)
+                TableCell("Local", columnWeight)
+            }
+        }
+        items(tableData) {
+            val (id, text) = it
+            Row(Modifier.fillMaxWidth()) {
+                TableCell(painterResource(id = R.drawable.info), columnWeight)
+                TableCell(id.toString(), columnWeight)
+                TableCell(text, columnWeight)
+                TableCell(text, columnWeight)
+            }
+        }
     }
 }
 
